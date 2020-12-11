8a94e713937a25b01cb3dc44f5e45728d32ccb00
Lu Hao <haolu1125@gmail.com>
2020-12-11 00:35:57 -0500

    dynamic time for scheduler

---
+++ script.js
@@ -0,0 +1,10 @@
+//show current day and time
+
+var dynamicTime = function () {
+    document.getElementById("currentDay")
+        .innerHTML = moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(dynamicTime, 1000);
+
+
+
