63ae5115e8b43c8a4102e113b7c6e86b2df99c22
Lu Hao <haolu1125@gmail.com>
2020-12-13 21:54:45 -0500

    set initial schedule

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+var currentDay = document.getElementById("currentDay");
+var scheduleArea = document.getElementById("schedule");
+var timeBlocks = document.getElementById("calendar-row");
 
-var test = document.getElementsByClassName("text-entry1");
-console.log(test);
+var currentDate = moment().format("dddd, MMMM Do");
+var currentHour = moment().format("H");
 
-var test2 = document.getElementById("text-entry1");
-console.log(test2);
+console.log(currentDate);
+console.log(currentHour);
 
-var test3 = document.querySelectorAll("#text-entry1");
-console.log(test3);
+var savedItem = [];
+var scheduleInput = [];
 
 //show current day and time
 var dynamicTime = function () {
@@ -15,22 +18,53 @@ var dynamicTime = function () {
 }
 setInterval(dynamicTime, 1000);
 
-//save function for the saveBtn
 
-localStorage.setItem("savedItem", JSON.stringify(savedItem));
+function init() {
+    //for each time block
+    timeBlocks.each(function () {
+        var thisBlockHr = parseInt($(this).attr("data-hour"));
+        var scheduleInput = {
+            //set related todo hour to same as data-hour
+            hour: thisBlockHr,
+            //get text ready to accept string input
+            text: "",
+        };
+        //add this todo object to todoitems array
+        savedItem.push(scheduleInput);
+    });
 
-$(".saveBtn").on("click", function (event) {
-
-    event.preventDefault();
-    var scheduleInput = $(this).siblings(".text-entry").val();
-    console.log(scheduleInput);
-    var id = $(this).attr('id');
-    var savedItem = [];
-    savedItem = JSON.parse(window.localStorage.getItem("scheduleInput")) || [];
-    savedItem.push(scheduleInput);
-    alert(savedItem);
     localStorage.setItem("savedItem", JSON.stringify(savedItem));
 
-});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
